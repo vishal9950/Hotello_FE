4ba93b7b38223141c7deb160e18367c7baa59f32
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-02-25 22:37:43 +0530

    Add Header component with styles

---
+++ src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <div className="Header-head">
+    <span className="Header-logo">Hotello</span>
+    <span className="Header-user"><i className="material-icons">person</i></span>
+  </div>
+);
+
+export default Header;
