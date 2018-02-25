8bcb70b521eab2f4e2f30beb8fa1126c28ccfc7c
aakash_raj <aakash.raj5@gmail.com>
2018-02-25 13:23:23 +0530

    Changed Folder structure

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "env": {
+        "jest": true,
+        "browser": true
+    },
+    "rules": {
+        "no-console": 0,
+        "no-underscore-dangle": 0,
+        "react/forbid-prop-types": 0
+    }
+};
