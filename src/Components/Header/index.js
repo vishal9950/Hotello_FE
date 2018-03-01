781e0962d401bcf9270ea5356f29b0f9ceecc15a
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-01 22:11:13 +0530

    Add dropdown for user icon

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => (
-  <div className="Header-head">
-    <span className="Header-logo">Hotello</span>
-    <span className="Header-user"><i className="material-icons">person</i></span>
-  </div>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDropdownOpen: false,
+    };
+  }
+
+  onClickHandler = () => {
+    this.setState({
+      isDropdownOpen: !this.state.isDropdownOpen,
+    });
+  }
+
+  render() {
+    return (
+      <div className="Header-head">
+        <span className="Header-logo">Hotello</span>
+        <span className="Header-user">
+          <button className="Header-drop-usr-btn" onClick={() => { this.onClickHandler(); }}>
+            <i id="Header-user-icon" className="material-icons">person</i>
+          </button>
+        </span>
+        <span className={this.state.isDropdownOpen ? 'Header-drop-opened' : 'Header-drop-closed'}>
+          <div className="Header-dropdown-items">
+            Your Profile
+          </div>
+          <div className="Header-dropdown-line" />
+          <div className="Header-dropdown-items">
+            Logout
+          </div>
+        </span>
+      </div>
+    );
+  }
+}
 
 export default Header;
