74866a736bdbdd14b20116a5df5012a6aef7dec1
sreekar <sreekar339@gmail.com>
2018-03-05 19:36:17 +0530

    nedd css fixes

--- src/Components/MainHeader/index.js
+++ src/Components/MainHeader/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import '../ActionBar/ActionBar.css';
 import IconButton from '../IconButton';
 import InputText from '../InputText';
-import ActionBar from '../ActionBar';
 import './MainHeader.css';
 
 class MainHeader extends React.Component {
-  w3Open = () => {
+  openSidebar = () => {
     this.props.changeStyle({ display: 'block' });
   }
   render() {
     return (
       <div className="w3-main" style={{ marginLeft: '200px' }}>
         <div className="foo">
-          <button className="w3-button w3-xlarge w3-hide-large" onClick={this.w3Open}>&#9776;</button>
+          <button className="w3-button w3-xlarge w3-hide-large" onClick={this.openSidebar}>&#9776;</button>
           <IconButton type="add" />
           <IconButton type="filter_list" />
           <InputText placeholder="Search..." />
--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import './Sidebar.css';
 
 class Sidebar extends React.Component {
-  w3Close = () => {
+  closeSideBar = () => {
     this.props.changeStyle({ display: 'none' });
   }
   render() {
     return (
       <div className="w3-sidebar w3-bar-block w3-collapse w3-animate-left" style={{ ...this.props.sidebarStyle, width: '200px' }}>
-        <button className="w3-bar-item w3-button w3-large w3-hide-large" onClick={this.w3Close}>Close &times;</button>
-        <Link to="/manageUsers"><button className="linky" onClick={this.w3Close}>Manage Users</button></Link>
-        <Link to="/manageBookings"><button className="linky" onClick={this.w3Close}>Manage Booking</button></Link>
+        <button className="w3-bar-item w3-button w3-large w3-hide-large" onClick={this.closeSideBar}>Close &times;</button>
+        <Link to="/manageUsers"><button className="linky" onClick={this.closeSideBar}>Manage Users</button></Link>
+        <Link to="/manageBookings"><button className="linky" onClick={this.closeSideBar}>Manage Booking</button></Link>
       </div>
     );
   }
