76e7b1999f7b58fae0cc7beab7b602f0b8404c16
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-05 19:30:26 +0530

    Add Admin Profile component

---
+++ src/Components/AdminProfile/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './AdminProfile.css';
+
+class AdminProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      passwordBoxText: 'Change Password',
+      isChanging: false,
+    };
+  }
+
+  onClickHandler = () => {
+    if (!this.state.isChanging) {
+      this.setState({
+        passwordBoxText: 'Close x',
+        isChanging: true,
+      });
+    } else {
+      this.setState({
+        passwordBoxText: 'Change Password',
+        isChanging: false,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="AdminProfile-head">
+              Your Profile
+        </div>
+
+        <div className="AdminProfile-outer">
+          <div className="AdminProfile-thumb">
+            AV
+          </div>
+          <div className="AdminProfile-details">
+            <div className="APDetails-name">*First Name* *Last Name*</div>
+            <div className="APDetails-phone">*Phone Number*</div>
+            <div className="APDetails-email">*Email*</div>
+            <div className="APDetails-pswd">
+              <button className="APDetails-btn" onClick={() => { this.onClickHandler(); }}>{this.state.passwordBoxText}</button>
+            </div>
+            <div className={this.state.isChanging ? 'APDetails-boxShow' : 'APDetails-boxNoShow'}>
+              <div className="APDetails-labelContent"><label className="APDetails-label">Old Password </label><input type="password" className="APDetails-input" /></div>
+              <div className="APDetails-labelContent"><label className="APDetails-label">New Password </label><input type="password" className="APDetails-input" /></div>
+              <div className="APDetails-labelContent"><label className="APDetails-label">Confirm Password </label><input type="password" className="APDetails-input" /></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AdminProfile;
